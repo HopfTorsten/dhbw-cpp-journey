bd9e9cf1dacd63e1a3167c2d98437a340ba23e7b
Torsten Hopf <torsten.hopf@mhp.com>
2018-11-29 17:00:07 +0100

    Fixed a bug with the CLI

--- day8/gen_bin.cpp
+++ day8/gen_bin.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+
 void writeMessage(std::ofstream &stream, char *msg, int16_t size)
 {
     if (!stream.is_open())
@@ -8,7 +9,6 @@ void writeMessage(std::ofstream &stream, char *msg, int16_t size)
         std::runtime_error err("Shit");
         throw err;
     }
-
     stream.write((char *)&size, sizeof(int16_t));
     std::reverse(msg, msg + size);
     for (int i = 0; i < size; ++i)
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 {
     int x = 100;
     std::string fileName = "bin_out";
-    if (argc == 2)
+    if (argc > 1)
     {
         x = atoi(argv[1]);
     }
@@ -35,7 +35,7 @@ int main(int argc, char **argv)
         std::ofstream ofstream(fileName.c_str());
         for (auto i = 0; i < x; i++)
         {
-            std::cin.getline(buf, 200);
+            std::cin.getline(buf, 255);
             auto str_len = strlen(buf);
             writeMessage(ofstream, buf, str_len);
         }
